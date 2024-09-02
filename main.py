ded871a83be4f378a16a96452a4667ba4ca62aa8
sashank <sashanktalakola2@gmail.com>
2024-09-02 13:37:23 -0400

    scraping searched properties

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
 import asyncio
 from playwright.async_api import async_playwright
+from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 load_dotenv()
 
-USERNAME = os.getenv("USER_NAME")
+# Loading env variables
+USERNAME = os.getenv("USER_NAME") # Don't use USERNAME env variable. It represents logged in linux user.
 PASSWORD = os.getenv("PASSWORD")
 HOST = os.getenv("HOST")
 PORT = os.getenv("PORT")
 
 
+# Brightdata authentication data
 AUTH = f'{USERNAME}:{PASSWORD}'
 SBR_WS_CDP = f'wss://{AUTH}@{HOST}:{PORT}'
 
 HOMEPAGE_URL = "https://zoopla.co.uk"
 
+# Main scraper function
 async def run(pw):
-    print('Connecting to Scraping Browser...')
-    browser = await pw.chromium.connect_over_cdp(SBR_WS_CDP)
+    logging.debug('Connecting to Scraping Browser...')
+    # browser = await pw.chromium.connect_over_cdp(SBR_WS_CDP)
+    browser = await pw.chromium.launch(headless=False)
     try:
         page = await browser.new_page()
-        print(f'Connected! Navigating to {HOMEPAGE_URL}')
+        logging.debug(f'Connected! Navigating to {HOMEPAGE_URL}')
         await page.goto(HOMEPAGE_URL)
 
-        print("Navigated! Scraping web page")
-        html = await page.content()
-        print(html)
+        # logging.debug("Navigated! Scraping web page")
+        # html = await page.content()
+        # print(html)
+
+        # Property location input
+        await page.fill('input[name="autosuggest-input"]', "Glasgow")
+        await page.keyboard.press("Enter")
+        await page.wait_for_load_state("load")
+
+        logging.debug("Queried location - Glasgow")
+
+        regularListings = await page.inner_html('div[data-testid="regular-listings"]')
+        soup = BeautifulSoup(await page.content(), features="html.parser")
+
+        for i, listing in enumerate(soup.find_all("div", class_="dkr2t86")):
+            pass
+
 
     finally:
         await browser.close()
